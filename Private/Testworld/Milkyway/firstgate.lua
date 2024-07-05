b89a451b9c815673b3335418e937c8d015591a74
Insomniaek <92553624+Insomniaek@users.noreply.github.com>
2024-07-05 23:00:07 +0200

    Create firstgate.lua

---
+++ Private/Testworld/Milkyway/firstgate.lua
@@ -0,0 +1,54 @@
+address = {"Libra", "Equuleus", "Piscis Austrinus", "Sagittarius", "Capricornus", "Perseus", "Gemini", "Cancer", "Point of Origin"}
+
+c = require("component")
+event = require("event")
+os = require("os")
+sg = c.stargate
+
+print("Dialing")
+for i,v in ipairs(address) do print(i,v) end
+print()
+
+loop = true
+
+function dialNext(dialed)
+	glyph = address[dialed + 1]
+	print("Engaging "..glyph.."... ")
+
+	sg.engageSymbol(glyph)
+end
+
+function cancelEvents()
+	event.cancel(eventEngaged)
+	event.cancel(openEvent)
+	event.cancel(failEvent)
+	
+	print("Cancelled all event listeners")
+	
+	loop = false
+end
+
+eventEngaged = event.listen("stargate_spin_chevron_engaged", function(evname, address, caller, num, lock, glyph)	
+	os.sleep(0.5)
+		
+	if lock then		
+		print("Engaging...")
+		sg.engageGate()
+	else
+		dialNext(num)
+	end
+end)
+
+dialNext(0)
+
+openEvent = event.listen("stargate_open", function()
+	print("Stargate opened successfully")
+	cancelEvents()
+end)
+
+failEvent = event.listen("stargate_failed", function()
+	print("Stargate failed to open")
+	cancelEvents()
+end)
+
+while loop do os.sleep(0.1) end
